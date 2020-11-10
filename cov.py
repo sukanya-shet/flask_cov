b3a914149f4242db9345df2065756428ae6ebb42
sukanya-shet <sansu2817@gmail.com>
2020-11-11 02:39:58 +0530

    home added

--- cov.py
+++ cov.py
@@ -1,16 +1,22 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 from covid import Covid
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
+@app.route('/')
 def home():
-    d={}
+    return('<em><b>Hello world!</b></em>')
+
+
+@app.route('/Covid', methods=['GET'])
+def details():
+    d = {}
     covid = Covid()
-    country_name = 'India'
+    country_name = request.args['Country']
     cases = covid.get_status_by_country_name(country_name)
-    d[country_name] =cases['confirmed']
+    d[country_name] = cases['confirmed']
     return jsonify(d)
+
 
 if __name__ == '__main__':
     app.run()
